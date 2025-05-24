96734cd11e79f15e94e41bbd6ce78bf345dbb0c7
ollie dwyer <olliedwyer@gmail.com>
2025-05-24 14:20:08 +1000

    restaurant review

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: [:create]
+  end
+end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,43 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Seeding restaurants..."
+
+Restaurant.destroy_all
+
+Restaurant.create!(
+  name: "Epicure",
+  address: "75008 Paris",
+  phone_number: "01 45 62 11 20",
+  category: "french"
+)
+
+Restaurant.create!(
+  name: "Sushi Samba",
+  address: "Heron Tower, London",
+  phone_number: "020 3640 7330",
+  category: "japanese"
+)
+
+Restaurant.create!(
+  name: "Luigi's Pizzeria",
+  address: "Rome, Italy",
+  phone_number: "+39 06 9831234",
+  category: "italian"
+)
+
+Restaurant.create!(
+  name: "Brussels Bistro",
+  address: "Brussels, Belgium",
+  phone_number: "+32 2 512 34 56",
+  category: "belgian"
+)
+
+Restaurant.create!(
+  name: "Dragon Palace",
+  address: "Chinatown, Sydney",
+  phone_number: "+61 2 9264 1688",
+  category: "chinese"
+)
+
+puts "Done!"
